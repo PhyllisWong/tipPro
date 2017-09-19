0583a1357cec6f3b1654b1f86e31ff1028777b36
Phyllis Wong <wong.phyllism@gmail.com>
2017-09-19 11:29:19 -0700

    finished UI

--- tipPro/ViewController.swift
+++ tipPro/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var billAmountField: UITextField!
+	@IBOutlet weak var tipSelector: UISegmentedControl!
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
