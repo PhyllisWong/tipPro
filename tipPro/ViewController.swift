8a7a4f0e4588cd07043ca803f14d9e3031108e19
Phyllis Wong <wong.phyllism@gmail.com>
2017-09-19 12:13:28 -0700

    added logic, working code

--- tipPro/ViewController.swift
+++ tipPro/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var billAmountField: UITextField!
 	@IBOutlet weak var tipSelector: UISegmentedControl!
+	@IBOutlet weak var tipAmountTextField: UITextField!
+	@IBOutlet weak var totalAmountTextField: UITextField!
 	
 	
 	
@@ -25,6 +27,63 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	
+	@IBAction func calculateTip(_ sender: Any) {
+		// 
+		guard let billAmount = Double(billAmountField.text!) else {
+			// show error
+			billAmountField.text = ""
+			tipAmountTextField.text = ""
+			totalAmountTextField.text = ""
+			return
+		}
+		
+		var tipPercentage: Double = 0.0
+		
+		switch tipSelector.selectedSegmentIndex {
+		case 0:
+			tipPercentage = 0.15
+		case 1:
+			tipPercentage = 0.18
+		case 2:
+			tipPercentage = 0.20
+		default:
+			break
+		}
+		
+		let roundedBillAmount = round(100*billAmount)/100
+		let tipAmount = roundedBillAmount * tipPercentage
+		let roundedTipAmount = round(100*tipAmount)/100
+		let totalAmount = roundedBillAmount + roundedTipAmount
+		
+		if (!billAmountField.isEditing) {
+			billAmountField.text = String(format: "%.2f", roundedBillAmount)
+		}
+		
+		tipAmountTextField.text = String(format: "%.2f", roundedTipAmount)
+		totalAmountTextField.text = String(format: "%.2f", totalAmount)
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
